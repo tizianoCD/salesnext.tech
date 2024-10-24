b08120e2e7114bb98d1e107b0e47271e05bea2a9
TizianoC <115004877+tizianoCD@users.noreply.github.com>
2024-10-24 22:45:38 +0200

    css

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -178,13 +178,3 @@ function switchLanguage() {
   }
 }
 
-
-
-function toggleMenu() {
-  var menu = document.getElementById('hamburger-menu');
-  if (menu.style.display === 'none' || menu.style.display === '') {
-    menu.style.display = 'block';
-  } else {
-    menu.style.display = 'none';
-  }
-}
