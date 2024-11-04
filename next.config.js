9bd835f5d9340ca19e18cc5034940eb6e85c84c5
tizianoCD <tizianocandida@gmail.com>
2024-11-04 22:17:42 +0100

    t3

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
 const createNextIntlPlugin = require('next-intl/plugin');
-
+ 
 const withNextIntl = createNextIntlPlugin();
-
+ 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',  // Enable static exports
-  images: {
-    unoptimized: true, // Required for static export
-  },
-  basePath: '/webJs', // Replace with your repository name
-  assetPrefix: '/webJs/', // Replace with your repository name
-};
-
-
+const nextConfig = {};
+ 
 module.exports = withNextIntl(nextConfig);
