d3eab48c0aa9399f821e33752381e239a5ccc4fd
tizianoCD <tizianocandida@gmail.com>
2024-11-05 12:55:09 +0100

    c1

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
+const createNextIntlPlugin = require('next-intl/plugin');
+ 
+const withNextIntl = createNextIntlPlugin();
+ 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["localhost"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
-      },
-    ],
-  },
-};
-
-module.exports = nextConfig;
+const nextConfig = {};
+ 
+module.exports = withNextIntl(nextConfig);
